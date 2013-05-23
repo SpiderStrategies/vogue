fe86f1318e3b03b9ab5728dd2a574f01ee3151f9
Nathan Bowser <nathan.bowser@spiderstrategies.com>
2013-05-23 12:16:54 -0400

    Update nano slider

--- example/bundle.js
+++ example/bundle.js
@@ -290,7 +290,7 @@ function pageY(e) {
 
 module.exports = Vogue
 
-},{"backbone":1,"lodash":2,"nano-slider":3}],2:[function(require,module,exports){
+},{"backbone":1,"nano-slider":2,"lodash":3}],3:[function(require,module,exports){
 (function(global){/**
  * @license
  * Lo-Dash 1.2.1 (Custom Build) <http://lodash.com/>
@@ -5557,7 +5557,129 @@ module.exports = Vogue
 }(this));
 
 })(window)
-},{}],1:[function(require,module,exports){
+},{}],2:[function(require,module,exports){
+var Backbone = require('backbone')
+  , _ = require('lodash')
+
+function pageX(e) {
+  return e.type === 'touchmove' || e.type == 'touchstart' ? e.originalEvent.touches[0].pageX : e.pageX
+}
+
+var Handle = Backbone.View.extend({
+
+  className: 'slider-handle',
+
+  events: {
+    'mousedown': 'start',
+    'touchstart': 'start',
+    'click': 'click'
+  },
+
+  initialize: function () {
+    _.bindAll(this, 'move', 'end')
+  },
+
+  start: function (e) {
+    e.preventDefault()
+    e.stopPropagation()
+
+    $(document).on('mousemove touchmove', this.move)
+    $(document).on('mouseup touchend', this.end)
+
+    this.trigger('start')
+  },
+
+  move: function (e) {
+    e.stopPropagation()
+    var percentage = (pageX(e) - this.$el.parent().offset().left) / this.$el.parent().width()
+
+    percentage = Math.max(0, percentage)
+    percentage = Math.min(1, percentage)
+    if (percentage >= 0 && percentage <= 1) {
+      this.trigger('change', percentage)
+    }
+  },
+
+  position: function (value) {
+    this.$el.css('left', parseInt(value * 100, 10) + '%')
+  },
+
+  end: function (e) {
+    e.preventDefault()
+    $(document).off('mousemove touchmove gesturechange', this.move)
+    $(document).off('mouseup touchend gestureend', this.end)
+    this.trigger('end')
+  },
+
+  click: function (e) {
+    e.preventDefault()
+  }
+
+})
+
+var Slider = Backbone.View.extend({
+
+  className: 'slider',
+
+  template: '<div class="slider-small"></div>' +
+            '<div class="slider-bar-wrapper">' +
+            '  <div class="slider-bar"></div>' +
+            '</div>' +
+            '<div class="slider-big"></div>',
+
+  initialize: function () {
+    this.min = _.isUndefined(this.options.min) ? 0 : this.options.min
+    this.max = _.isUndefined(this.options.max) ? 100 : this.options.max
+    this.value = _.isUndefined(this.options.value) ? this.min : this.options.value
+    this.distance = this.max - this.min + 1
+    this.step = _.isUndefined(this.options.step) ? 1 : this.options.step
+    this.handle = new Handle
+    this.handle.on('change', this.change, this)
+    this.handle.on('start', this.startSlide, this)
+    this.handle.on('end', this.stopSlide, this)
+
+    if (this.min > this.max) {
+      throw new Error('Unsupported range')
+    }
+  },
+
+  render: function () {
+    this.$el.html(this.template)
+    this.$('.slider-bar').append(this.handle.el)
+    this.change((this.value - this.min) * (1 / (this.max - this.min)))
+    return this
+  },
+
+  startSlide: function () {
+    this.trigger('startSlide')
+  },
+
+  stopSlide: function () {
+    this.trigger('stopSlide')
+  },
+
+  change: function (percentage) {
+    var prev = this.value
+    this.value = percentage / (1 / (this.max - this.min)) + this.min
+
+    if (this.step) {
+      this.value = Math.round(this.value / this.step) * this.step
+    }
+
+    if (prev !== this.value) {
+      this.trigger('change', this.value)
+    }
+
+    this.trigger('slide')
+
+    this.handle.position((this.value - this.min) * (1 / (this.max - this.min)))
+  }
+
+})
+
+module.exports = Slider
+
+},{"backbone":1,"lodash":3}],1:[function(require,module,exports){
 (function(){//     Backbone.js 1.0.0
 
 //     (c) 2010-2013 Jeremy Ashkenas, DocumentCloud Inc.
@@ -7131,103 +7253,5 @@ module.exports = Vogue
 }).call(this);
 
 })()
-},{"lodash":2}],3:[function(require,module,exports){
-var Backbone = require('backbone')
-  , _ = require('lodash')
-
-function pageX(e) {
-  return e.type === 'touchmove' || e.type == 'touchstart' ? e.originalEvent.touches[0].pageX : e.pageX
-}
-
-var Handle = Backbone.View.extend({
-
-  className: 'slider-handle',
-
-  events: {
-    'mousedown': 'start',
-    'touchstart': 'start',
-    'click': 'click'
-  },
-
-  initialize: function () {
-    _.bindAll(this, 'move', 'end')
-  },
-
-  start: function (e) {
-    e.preventDefault()
-    e.stopPropagation()
-
-    $(document).on('mousemove touchmove', this.move)
-    $(document).on('mouseup touchend', this.end)
-  },
-
-  move: function (e) {
-    e.stopPropagation()
-    var percentage = (pageX(e) - this.$el.parent().offset().left) / this.$el.parent().width()
-    if (percentage > 0 && percentage < 1) {
-      this.trigger('change', percentage)
-    }
-  },
-
-  position: function (value) {
-    this.$el.css('left', parseInt(value * 100, 10) + '%')
-  },
-
-  end: function (e) {
-    e.preventDefault()
-    $(document).off('mousemove touchmove gesturechange', this.move)
-    $(document).off('mouseup touchend gestureend', this.end)
-  },
-
-  click: function (e) {
-    e.preventDefault()
-  }
-
-})
-
-var Slider = Backbone.View.extend({
-
-  className: 'slider',
-
-  template: '<div class="slider-small"></div>' +
-            '<div class="slider-bar-wrapper">' +
-            '  <div class="slider-bar"></div>' +
-            '</div>' +
-            '<div class="slider-big"></div>',
-
-  initialize: function () {
-    this.min = _.isUndefined(this.options.min) ? 0 : this.options.min
-    this.max = _.isUndefined(this.options.max) ? 100 : this.options.max
-    this.value = _.isUndefined(this.options.value) ? this.min : this.options.value
-    this.distance = this.max - this.min + 1
-    this.step = _.isUndefined(this.options.step) ? 1 : this.options.step
-    this.handle = new Handle
-    this.handle.on('change', this.change, this)
-
-    if (this.min > this.max) {
-      throw new Error('Unsupported range')
-    }
-  },
-
-  render: function () {
-    this.$el.html(this.template)
-    this.$('.slider-bar').append(this.handle.el)
-    this.change(this.value / this.distance)
-    return this
-  },
-
-  change: function (percentage) {
-    this.value = this.distance * percentage
-    if (this.step) {
-      this.value = Math.round(this.value / this.step) * this.step
-    }
-    this.trigger('change', this.value)
-    this.handle.position(this.value / this.distance)
-  }
-
-})
-
-module.exports = Slider
-
-},{"backbone":1,"lodash":2}]},{},[])
+},{"lodash":3}]},{},[])
 ;
